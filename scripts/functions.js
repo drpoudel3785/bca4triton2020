6ca0a4d9639b727b224d3d30dc60058593a973fd
drpoudel3785 <dharmarajpoudel@hotmail.com>
2020-09-29 08:21:57 +0545

    Cookie and Unicode

--- scripts/functions.js
+++ scripts/functions.js
@@ -186,26 +186,23 @@ function admissionOpen(){
   window.open('/admissionopen.html','popUpWindow','height=470,width=960,left=100,top=100,resizable=no,scrollbars=no,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
 }
 
-
-
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+// When page loads, open the modal 
+document.body.onload = function() {
+  modal.style.display = "block";
+}
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
